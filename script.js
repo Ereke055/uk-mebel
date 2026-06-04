f12f4b5cc79d6fc4e9146ec00ef071507af1e734
Ereke055 <akkulerkebulan27@gmail.com>
2026-06-04 16:54:55 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const links = document.querySelectorAll('nav a');
+
+links.forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    const section = document.querySelector(this.getAttribute('href'));
+
+    section.scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
